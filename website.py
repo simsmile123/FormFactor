aaed59bef3c0d0d09252f7566055c9acface23c7
shripadakumalla <109306265+shripada2005@users.noreply.github.com>
2024-03-22 16:20:27 -0400

    Update website.py

--- website.py
+++ website.py
@@ -5,7 +5,7 @@ st.set_page_config(
     layout="centered",
     initial_sidebar_state="expanded"
 )
-
+#opens a file and allows video uploading :)
 st.title("Form Factor")
 st.write("Path to Perfection")
 st.divider()
